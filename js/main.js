e53ba4b15f4d76d9a0c7343e1ade446797348342
Michele <micheleierniero@gmail.com>
2021-07-15 15:12:56 +0200

    prompt

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Quanti chilometri?
+
+var howLong
+
+howLong = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+
+console.log(howLong);
+
+
+// Quanti anni hai?
+
+var yourAge
+
+yourAge = parseInt(prompt("Quanti anni hai?"));
+
+console.log(yourAge);
+
+
+
+
+
+
+
+
